0ddd4d185780dcf60e32f5374db1b832d4abdaf8
Connor Lafata <connorlafata@gmail.com>
2017-02-24 19:35:19 -0500

    Mostly finished app

---
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Connor Lafata on 2/23/17.
+//  Copyright © 2017 Connor Lafata. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+    
+    @IBOutlet weak var definitionLabel: UILabel!
+    @IBOutlet weak var emojiLabel: UILabel!
+    
+    var emoji = "No Emoji"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        emojiLabel.text = emoji
+        
+        if emoji == "😀" {
+            definitionLabel.text = "A grinning face"
+        }
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
